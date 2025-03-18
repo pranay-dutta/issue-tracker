10d4e62c9ef4bae02ca3b91f1edfec1cfaf68fc6
Pranay Dutta <rick15176@gmail.com>
2025-03-19 01:41:34 +0530

    Refactor: Apply the SRP

---
+++ app/issues/[id]/IssueDetails.tsx
@@ -0,0 +1,21 @@
+import { IssueStatusBadge } from "@/app/components";
+import { Issue } from "@prisma/client";
+import { Heading, Flex, Card, Text } from "@radix-ui/themes";
+import Markdown from "react-markdown";
+
+const IssueDetails = ({ issue }: { issue: Issue }) => {
+  return (
+    <>
+      <Heading>{issue.title}</Heading>
+      <Flex my="2" gap="4">
+        <IssueStatusBadge status={issue.status} />
+        <Text>{issue.createdAt.toDateString()}</Text>
+      </Flex>
+      <Card className="prose mt-4">
+        <Markdown>{issue.description}</Markdown>
+      </Card>
+    </>
+  );
+};
+
+export default IssueDetails;
