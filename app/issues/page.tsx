3744a0442ad4c86dd8a503be3707a9048afd64bb
Pranay Dutta <rick15176@gmail.com>
2025-03-18 15:03:57 +0530

    Build a custom link component

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,9 +1,9 @@
 import prisma from "@/prisma/client";
 import { Table } from "@radix-ui/themes";
+import Link from "@/app/components/Link";
 import delay from "delay";
 import IssueStatusBadge from "../components/IssueStatusBadge";
 import IssueActions from "./IssueActions";
-import Link from "next/link";
 
 const IssuePage = async () => {
   const issues = await prisma.issue.findMany();
@@ -32,6 +32,7 @@ const IssuePage = async () => {
               <Table.Row key={issue.id}>
                 <Table.Cell>
                   <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
+
                   <div className="block md:hidden">
                     <IssueStatusBadge status={issue.status} />
                   </div>
