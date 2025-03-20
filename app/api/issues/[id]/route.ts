1e2c081f40abf6ce20ba4505f7a0ec44edf1d4fd
Pranay Dutta <rick15176@gmail.com>
2025-03-21 01:06:53 +0530

    Improve the user experience

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,6 +1,6 @@
-import { NextRequest, NextResponse } from "next/server";
 import { issueSchema } from "@/app/validationSchemas";
 import prisma from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 
 interface Props {
--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { AlertDialog, Button, Flex } from "@radix-ui/themes";
+import { Spinner } from "@/app/components";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
@@ -7,12 +8,16 @@ import { useState } from "react";
 const DeleteIssueButton = ({ issueId }: { issueId: string }) => {
   const router = useRouter();
   const [error, setError] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
+
   const deleteIssue = async () => {
     try {
+      setIsDeleting(true);
       await axios.delete("/api/issues/" + issueId);
       router.push("/issues");
     } catch {
       setError(true);
+      setIsDeleting(false);
     }
   };
 
@@ -20,8 +25,9 @@ const DeleteIssueButton = ({ issueId }: { issueId: string }) => {
     <>
       <AlertDialog.Root>
         <AlertDialog.Trigger>
-          <Button color="red" style={{ width: "100%" }}>
+          <Button color="red" style={{ width: "100%" }} disabled={isDeleting}>
             Delete Issue
+            {isDeleting && <Spinner />}
           </Button>
         </AlertDialog.Trigger>
         <AlertDialog.Content>
